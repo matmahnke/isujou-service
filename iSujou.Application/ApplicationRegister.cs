d84041a2279a4d097028e18f90ab4cf20749d8b0
matheus mahnke <matheusmahnke@gmail.com>
2020-03-08 00:49:26 -0300

    initial project structure

---
+++ iSujou.Application/ApplicationRegister.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace iSujou.Application
+{
+    public static class ApplicationRegister
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection service)
+        {
+            return service;
+        }
+    }
+}
---
+++ iSujou.Infra/iSujouContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iSujou.Infra
+{
+    public class iSujouContext : DbContext
+    {
+        public iSujouContext(DbContextOptions<iSujouContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("DefaultConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
