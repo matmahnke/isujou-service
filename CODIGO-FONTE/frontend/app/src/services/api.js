80dd6e50a1c9bd1bf2bccfcd429413b1b29bdc54
Alan Jantz <BRHBS08841@ambev.com.br>
2020-06-02 17:52:07 -0300

    Matheus pediu pra eu subir senao ele me batia dps da aula

--- CODIGO-FONTE/frontend/app/src/services/api.js
+++ CODIGO-FONTE/frontend/app/src/services/api.js
@@ -4,7 +4,8 @@ const Api = () => {
   const defaultOptions = {
     baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
-      'Content-type': 'application/json'
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('Authorization')
     }
   }
   return Axios.create(defaultOptions)
