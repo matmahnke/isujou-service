00b85ad44d4ff4d8689a23043d0ada30b3e7780c
Alan Jantz <BRHBS08841@ambev.com.br>
2020-06-29 23:23:14 -0300

    Enum conquistas

--- CODIGO-FONTE/backend/iSujou.Domain/Enums/Achievements.cs
+++ CODIGO-FONTE/backend/iSujou.Domain/Enums/Achievements.cs
@@ -1,9 +1,32 @@
-﻿namespace iSujou.Domain.Enums
+﻿using System.ComponentModel;
+
+namespace iSujou.Domain.Enums
 {
     public enum Achievements
     {
-        lvl_1,
-        lvl_2,
-        lvl_3
+        [Description("Tudo brilhando!")]
+        TudoBrilhando = 1,
+        [Description("Boa limpeza")]
+        BoaLimpeza = 2,
+        [Description("Limpeza das louças")]
+        LimpezaDasLoucas = 3,
+        [Description("Cuidadoso")]
+        Cuidadoso = 4,
+        [Description("Limpeza da lavanderia")]
+        LimpezaDaLavanderia = 5,
+        [Description("Bons materiais")]
+        BonsMateriais = 6,
+        [Description("Limpeza do banheiro")]
+        LimpezaDoBanheiro = 7,
+        [Description("Limpeza das roupas")]
+        LimpezaDasRoupas = 8,
+        [Description("Cuidado pessoal")]
+        CuidadoPessoal = 9,
+        [Description("Bom negócio")]
+        BomNegocio = 10,
+        [Description("Bom preço")]
+        BomPreco = 11,
+        [Description("Simpático")]
+        Simpatico = 12
     }
 }
